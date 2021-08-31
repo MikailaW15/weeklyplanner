875c56f022e740f9ae1e40905c8f463f4df6f4f4
Mikaila Williams <babysis@ymail.com>
2021-08-31 18:26:48 -0400

    updated script

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var clock = $('#currentDay')
+var time = moment().format("h:mm a")
+
 
 function updateClock() {
     var now = moment().format("dddd, MMMM Do YYYY, h:mm a")
@@ -6,3 +8,22 @@ function updateClock() {
 }
 
 setInterval(updateClock, 1000)
+
+function color() {
+    var now = new moment().format("h:mm a");
+    var n = now.getUTCHours() + 1;
+    if (n > now || n < 9) {
+        // If time is after 6PM or before 9AM, apply night theme to 'body'
+        $("input").addClass("future").removeClass("past");
+    } else {
+        // Else use ‘day’ theme
+        $("input").addClass("past").removeClass("future");
+    }
+}
+$(document).ready(function () {
+    color();
+});
+if (input < now) {
+    var now = moment().format("h:mm a")
+
+}
